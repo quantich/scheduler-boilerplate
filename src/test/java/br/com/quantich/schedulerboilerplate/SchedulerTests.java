ce52a9d115c01569994dcec35f77b1b72d4d0c83
Paulo Ricardo <paulor1809@gmail.com>
2018-10-08 10:23:55 -0300

    Change settings

--- src/test/java/br/com/quantich/schedulerboilerplate/SchedulerTests.java
+++ src/test/java/br/com/quantich/schedulerboilerplate/SchedulerTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PluggtoMiddlewareApplicationTests {
+public class SchedulerTests {
 
 	@Test
 	public void contextLoads() {
