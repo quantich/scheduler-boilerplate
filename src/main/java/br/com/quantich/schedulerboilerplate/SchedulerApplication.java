12530267c2afb2e9eda21914f21823eba1fcfedd
Paulo Ricardo <paulor1809@gmail.com>
2018-10-09 17:08:17 -0300

    Add multi-db

--- src/main/java/br/com/quantich/schedulerboilerplate/SchedulerApplication.java
+++ src/main/java/br/com/quantich/schedulerboilerplate/SchedulerApplication.java
@@ -2,13 +2,34 @@ package br.com.quantich.schedulerboilerplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
 public class SchedulerApplication {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
-	public static void main(String[] args) {
+    @Bean
+    @ConfigurationProperties(prefix="spring.second-datasource")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(SchedulerApplication.class, args);
 	}
+
+
+
 }
